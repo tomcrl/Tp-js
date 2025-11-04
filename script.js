35194b08006101e94059c494b2be3627c84e7d91
tomcrl <tom.caroleo@yahoo.com>
2025-11-04 15:11:48 +0000

    feat: Maîtrise des Objets JS

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let age = 17;
 // nom = "Tom"
 age = 18;
 console.log(age);
+
 const motDePasseAttendu = "secret123";
 let motDePasseUtilisateur = "12345";
 motDePasseUtilisateur = "secret123"
@@ -14,3 +15,17 @@ if (motDePasseUtilisateur === motDePasseAttendu) {
 } else {
     console.log("Accès refusé. Mot de passe incorrect.");
 }
+const moi = {
+    prenom: "Tom",
+    nom: "Caroleo",
+    age: 17,
+    phrase:  "Je m'appelle Tom Caroleo.",
+    competences: ["HTML", "CSS"]
+};
+console.log(moi);
+console.log(moi.prenom);
+console.log(moi.phrase);
+moi.age = 19;
+moi.ville = "paris";
+console.log(moi);
+
